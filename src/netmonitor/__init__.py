656d9a82d3e57691a4a282af21b1b1fe2def9464
Janne Parkkila <japskua@gmail.com>
2011-07-14 10:32:22 +0300

    Created the monitor package to start working on

---
+++ src/netmonitor/__init__.py
@@ -0,0 +1,1 @@
+from dbus import SystemBus
---
+++ src/netmonitor/monitor.py
@@ -0,0 +1,18 @@
+'''
+Created on Jul 14, 2011
+
+@author: Janne Parkkila
+@contact: japskua@gmail.com
+'''
+
+class Monitor(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        print "hello"
