415d35785645f88a228a96a850c2bb472d177b42
Janne Parkkila <japskua@gmail.com>
2011-08-24 09:38:22 +0300

    Created the reader.py to be a class instead of just procedural code

--- src/reader.py
+++ src/reader.py
@@ -11,6 +11,33 @@ from pipe import Pipe
 from time import sleep
 import sys
 
+
+class PipeReader(object):
+    """
+    The PipeReader class
+    """
+    _pipeName = None
+    _pipe = None
+    
+    def __init__(self, pipeName):
+        """
+        The Class Constructor
+        """
+        try:
+            self._pipeName = pipeName
+            self._pipe = Pipe(self._pipeName, "READ")
+        except Exception, instance:
+            print "Exception at creating the pipereader: %s" % instance
+
+    def Read():
+        """
+        @summary: This function reads the in-pipe
+                  for the latest information
+        @return: Returns the data from the pipe
+        """
+        return self._pipe.Read()
+        
+
 ## GLOBALS ##
 PIPENAME = "monitorpipe"
 
@@ -20,7 +47,8 @@ if __name__ == "__main__":
     if "-u" in sys.argv:
         update = int(sys.argv[sys.argv.index("-u") + 1])
 
-    pipein = Pipe(PIPENAME, "READ")
+   # pipein = Pipe(PIPENAME, "READ")
+    pipein = PipeReader(PIPENAME)
 
     while True:
         print "READER:", pipein.Read()
